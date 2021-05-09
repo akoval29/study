2a9ff90211629db42446f9bb312a38884a1d4b51
Dron <akoval29@gmail.com>
2021-05-10 01:44:38 +0300

    training1

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+"use strict";
+// let number = 1;
+// console.log(number);
+// number = 10,
+// console.log(number);
+
+// {
+//     var rez=5000;
+// }
+// console.log(rez);
+
+// console.log(4/0);
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answers = [];
+// answers[0]=prompt("как ваше имя", '');
+// answers[1]=prompt("your age?", '');
+// answers[2]=prompt("your surname?", '');
+// document.write(answers);
+// let incr=10,
+//     decr=10;
+// incr++
+// decr--
+// console.log(incr, decr);
+    
+const is = false,
+      as = false,
+      zz = true;
+
+console.log(is || as || zz);
