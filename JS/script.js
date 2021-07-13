a5f4c052b7ed1c38d773e5e3990beae0a22cccee
Dron <akoval29@gmail.com>
2021-07-13 23:35:28 +0300

    `debugger`

--- JS/script.js
+++ JS/script.js
@@ -229,28 +229,36 @@
 // console.log (werehouse['boxes']['boxOfChocolate']);
 // console.log (werehouse['boxes']['boxOfCandies']);
 
-///////////////////////////////////////////////////////////////////////////////////////////
+/////
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    }
-};
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     }
+// };
 
-for (let key in options) {
-    if (typeof(options[key])==='object') {
-        for (let i in options[key]) {
-            console.log(`свойство ${i} имеет значение ${options[key][i]}`);
-        }    
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-    }
+// for (let key in options) {
+//     if (typeof(options[key])==='object') {
+//         for (let i in options[key]) {
+//             console.log(`свойство ${i} имеет значение ${options[key][i]}`);
+//         }    
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+// }
+// console.log(Object.keys(options).length);
+// const {border, bg} = options.colors;
+// console.log(border);
+// console.log(bg);
+
+/////1.22
+
+function hello () {
+    console.log(`Hello`);
+    debugger;
 }
-console.log(Object.keys(options).length);
-const {border, bg} = options.colors;
-console.log(border);
-console.log(bg);
+hello();
